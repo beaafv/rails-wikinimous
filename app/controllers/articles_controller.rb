4740056f7648923d8bdff2238c44b7e4640c6f9c
Beatriz Vieira <beaferreirav@gmail.com>
2023-08-15 20:02:42 +0100

    with bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,42 @@
 class ArticlesController < ApplicationController
-  resources :articles
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  # def find_id
+  #   @article = Article.find(params[:id])
+
+  # end
+
 end
